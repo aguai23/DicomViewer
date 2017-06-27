8604f4fe0c0e984c418561e9f733f7c6984301fe
mhongzhang <mhongzhang@infervision.com>
2017-06-27 17:32:35 +0800

    add some style

--- server/config.js
+++ server/config.js
@@ -6,10 +6,10 @@ exports.Config = function () {
 
     this.host = "127.0.0.1";
     //dir name store files	
-    this.dirname = "/media/tx-eva-11/data/data/";
+    this.dirname = "/media/tx-eva-08/data/data/";
     //server port
     this.port = 3600;
     //data server port number
-    this.dataPort = 4600;
+    this.dataPort = 8600;
 
 }
